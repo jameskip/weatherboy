15484d189f0cdb63a2eb06974dd1fd769a705d86
jameskip <me@jameskip.com>
2021-01-14 16:53:23 -0600

    Add cors proxy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Current from "/Users/jk/Github/weatherboy/src/components/current/Current.tsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Current />
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/current/Current.tsx
+++ src/components/current/Current.tsx
@@ -1,16 +1,40 @@
-import React from "react"; // we need this to make JSX compile
+import React, { useState, useEffect } from "react";
 
-type CurrentConditionsCard = {
-  title: string;
-  paragraph: string;
+const CurrentConditionsCard = () => {
+  const [city, setCity] = useState<string>("");
+  const [key, setKey] = useState<string>("");
+
+  const handleChange = (e: any) => {
+    e.preventDefault();
+    setCity(e.target.value);
+  };
+
+  useEffect(() => {
+    const getLocationURL = `https://cors-anywhere.herokuapp.com/http://dataservice.accuweather.com/locations/v1/cities/search?apikey=k84As5CjO9g6RYbbFflBgIM7TM0ZHTDp&q=${city}&language=en-us`;
+
+    if (city) {
+      fetch(getLocationURL)
+        .then((res) => res.json())
+        .then((response) => {
+          setCity(response[0].EnglishName);
+          setKey(response[0].Key);
+          return;
+        })
+        .catch((err) => console.error(err));
+    }
+  }, [city]);
+
+  return (
+    <aside>
+      <input onChange={handleChange} type="text"></input>
+      {(city && (
+        <>
+          <h2>{city}</h2>
+          <h3>{key}</h3>
+        </>
+      )) || <p>Please enter a valid city.</p>}
+    </aside>
+  );
 };
 
-export const CurrentConditionsCard = ({
-  title,
-  paragraph,
-}: CurrentConditionsCard) => (
-  <aside>
-    <h2>{title}</h2>
-    <p>{paragraph}</p>
-  </aside>
-);
+export default CurrentConditionsCard;
